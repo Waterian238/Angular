90d0aea5969c34892433e43d501070aa7085ba57
Waterian238 <Waterian238@gmail.com>
2023-03-22 13:15:00 +0200

    .

---
+++ angular-Employees/src/app/employee-list.ts
@@ -0,0 +1,6 @@
+import { Employee } from "./employees/employee";
+
+export const EMPLOYEES:Employee[]=[
+    {id:2,name:'Ethan',surname:'Bartlett'},
+    {id:3,name:'Joshua',surname:'Anderson'}
+];
--- angular-Employees/src/app/employees/employees.component.ts
+++ angular-Employees/src/app/employees/employees.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { Employee } from './employee';
+import { EMPLOYEES } from '../employee-list';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent {
-employee:Employee={
-  id:1,
-  name:'Dewald',
-  surname:'Rinke'
-}
+
+Employee:EMPLOYEES;
 }
